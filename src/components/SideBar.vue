a09167090171747c2553abdc92151cfa014c7f2a
viicruz <vsouza@olie.ai>
2024-04-08 23:51:28 -0300

    feat- action to open and close sidebar

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 //Libraries Imports
 import { PhList } from "@phosphor-icons/vue";
+import { tv } from 'tailwind-variants';
 
 
 //Components Imports
@@ -12,17 +13,29 @@ import LibraryGameSection from "@/components/LibraryGameSection.vue";
 import {useSideBarStore} from "@/stores/sideBarSlice";
 
 const sideBarStore = useSideBarStore();
+
+const sideBarOpen = tv({
+  base:"h-full min-h-screen bg-secondary-200 p-2 gap-4 flex flex-col absolute left-0 top-0 z-10",
+  variants:{
+    open:{
+      true:"w-80",
+      false:"w-0"
+    }
+  }
+})
 </script>
 
 <template>
   <div
-    class="w-80 h-full min-h-screen bg-secondary-200 p-2 gap-4 flex flex-col absolute left-0 top-0 z-10"
+    :class="sideBarOpen({
+      open:sideBarStore.$state.isOpen
+    })"
   >
 
     <UserSectionComponent />
 
     <div class="absolute -right-8 rounded-r-lg pl-1 bg-secondary-200">
-      <button class="p-2">
+      <button class="p-2" @click="sideBarStore.toggle()">
         <PhList class="text-base text-black"/>
     </button>
     </div>
