397ddd9d102a7cca2aee4b11a34300145681eb7b
viicruz <vsouza@olie.ai>
2024-04-10 16:20:58 -0300

    feat- create FormInput component

---
+++ src/components/FormInput.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+defineProps({
+  label: String,
+});
+
+</script>
+<template>
+  <input
+    :placeholder="label"
+    class="bg-transparent focus:outline-none border focus:border-secondary-700 border-gray-300 rounded-md py-2 px-4 block w-full"
+  />
+</template>
--- src/components/Forms.vue
+++ src/components/Forms.vue
@@ -1,16 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import FormInput from "./FormInput.vue";
+</script>
 <template>
-  <form class="p-20 gap-2 flex flex-col rounded-lg bg-white ">
-
+  <form class="p-10 gap-2 flex flex-col rounded-lg bg-white">
     <div class="flex items-start justify-start bg-blue-700 pl-2">
       <h2 class="text-lg text-black font-bold">Buy a game</h2>
       <p></p>
     </div>
 
-    <div class="w-full bg-red-700 justify-center items-center flex flex-col">
-      <input type="text" placeholder="Name" />
-      <input type="email" placeholder="Email" />
-      <input type="password" placeholder="Password" />
+    <div class="w-full justify-center items-center flex flex-col gap-2">
+      <FormInput />
+      <FormInput label="R$50,00"/>
+      <FormInput label="username@gmail.com" />
       <button type="submit">Submit</button>
     </div>
   </form>
