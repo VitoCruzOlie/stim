2e0411c1154edec4f92d3cb178f9cbb255e3f460
viicruz <vsouza@olie.ai>
2024-04-10 21:08:49 -0300

    saving

--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -2,18 +2,14 @@
 import {
   DialogClose,
   DialogContent,
-  DialogDescription,
   DialogOverlay,
   DialogPortal,
   DialogRoot,
-  DialogTitle,
   DialogTrigger,
 } from "radix-vue";
 
 import Button from "@/components/Button.vue";
 import Forms from "@/components/Forms.vue";
-
-
 </script>
 <template>
   <DialogRoot>
@@ -21,16 +17,14 @@ import Forms from "@/components/Forms.vue";
       <Button :variants="{ color: 'primary' }" label="Buy the Game" />
     </DialogTrigger>
     <DialogPortal>
-      <DialogOverlay />
-      <DialogContent class="absolute left-0 top-0 w-full h-svh flex justify-center items-center z-10">
-       <Forms/>
+      <DialogOverlay
+        className="absolute top-0 left-0 w-full h-full bg-neutral-900/50 z-10 backdrop-blur-lg"
+      />
+      <DialogContent
+        class="absolute left-0 top-0 w-full h-svh flex justify-center items-center z-10"
+      >
+        <Forms />
       </DialogContent>
     </DialogPortal>
   </DialogRoot>
 </template>
-
-<!--
-<DialogDescription />
-<DialogClose />
-</DialogContent>
--->
--- src/components/Forms.vue
+++ src/components/Forms.vue
@@ -1,18 +1,31 @@
 <script setup lang="ts">
+//Component imports
 import FormInput from "./FormInput.vue";
+import Button from "./Button.vue";
 </script>
 <template>
-  <form class="p-10 gap-2 flex flex-col rounded-lg bg-white">
-    <div class="flex items-start justify-start bg-blue-700 pl-2">
+  <form class="py-2 px-10 gap-2 flex flex-col rounded-lg bg-white">
+    <div class="flex items-start justify-start">
       <h2 class="text-lg text-black font-bold">Buy a game</h2>
       <p></p>
     </div>
 
-    <div class="w-full justify-center items-center flex flex-col gap-2">
-      <FormInput />
-      <FormInput label="R$50,00"/>
-      <FormInput label="username@gmail.com" />
-      <button type="submit">Submit</button>
+    <div class="w-full justify-center items-center flex flex-col gap-2 text-neutral-600 text-sm">
+      <div class="w-full flex flex-col gap-1">
+        <span>Game name</span>
+        <FormInput />
+      </div>
+      <div class="w-full flex flex-col gap-1">
+        <span>Price</span>
+        <FormInput label="R$50,00" />
+      </div>
+
+      <div class="w-full flex flex-col gap-1 pb-4">
+        <span>Email</span>
+        <FormInput label="username@gmail.com" />
+      </div>
+
+      <Button label="Buy game" :variants="{ color: 'primary' }" />
     </div>
   </form>
 </template>
